fffa8ebde80d9878a134b084f4983643679ecba1
Brandon Phillips <brandon.phillips976@gmail.com>
2020-11-04 23:16:18 -0800

    copied base code from activity file

---
+++ public/webpack.config.js
@@ -0,0 +1,48 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './index.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget Tracker app',
+      short_name: 'Budget Tracker',
+      description: 'An application that allows you to keep track of your expenses and income even offline.',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('./dist/assets/icons/icon-192x192.png'),
+          sizes: [192, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
